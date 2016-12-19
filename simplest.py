8c19af866ae9397d8412c7f506afff5e5c57941f
Federico Stagni <federico.stagni@cern.ch>
2016-12-19 15:31:53 +0100

    added multithreaded and multiprocess versions

--- simplest.py
+++ simplest.py
@@ -1,10 +1,20 @@
-import xmlrpclib
+""" This is the simplest possible.
+
+    As it is now it is not equipped with Threads/Processes/coroutines whatever so it can also serve one call at a time
+"""
+
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+
+import fib
 
 def is_even(n):
   return n % 2 == 0
 
+def fib_func(n):
+  return fib.fib(n)
+
 server = SimpleXMLRPCServer(("localhost", 8000))
 print "Listening on port 8000..."
 server.register_function(is_even, "is_even")
+server.register_function(fib_func, "fib")
 server.serve_forever()
---
+++ simplest_threaded.py
@@ -0,0 +1,43 @@
+""" Simple multi-process xmlrpc server
+
+    No SSL (yet)
+"""
+
+import SocketServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+
+import fib
+
+#functions going to be exposed
+def fib_func(n):
+  return fib.fib(n)
+
+def fib_server(address = None):
+  """ Start a xmlrpc server
+  """
+
+  class SimpleThreadXMLRPCServer(SocketServer.ThreadingMixIn, SimpleXMLRPCServer):
+    pass
+  class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/TestSvc')
+
+  if address is None:
+    address = ("localhost", 8000)
+  server = SimpleThreadXMLRPCServer(address,
+                                    requestHandler=RequestHandler,
+                                    logRequests=True,
+                                    allow_none=True, )
+
+  print "Listening at %s/%d..." % (address[0], address[1])
+
+  server.register_function(fib_func, "fib")
+  server.serve_forever()
+
+
+fib_server()
+
+
+### To use client side:
+# import xmlrpclib
+# proxy = xmlrpclib.ServerProxy("http://localhost:8000/TestSvc")
+# proxy.fib(5)
